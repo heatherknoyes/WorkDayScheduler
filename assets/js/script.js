5598e1a45eb35829184e2666bc0e71bdfd4ec463
Heather Noyes <heatherknoyes@gmail.com>
2022-04-02 14:38:20 -0400

    Reduced code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,48 @@
 var currentDayEl = $("#currentDay");
+var calendarEl = $(".container");
+var now = moment().local();
+var startTime = 9;
+var endTime = 17;
 
-function setToday() {
-  var now = moment().local().format("dddd, MMMM Do");
-  currentDayEl.addClass("time-block");
-  currentDayEl.text(now);
+function createCalendar() {
+  for (var i = startTime; i <= endTime; i++) {
+    var tod = i + "AM";
+
+    if (i > 12) {
+      tod = i - 12 + "PM";
+    } else if (i === 12) {
+      tod = 12 + "PM";
+    }
+
+    calendarEl.append(`<div class="row">
+        <div class="col-1 hour">${tod}</div>
+        <div class="col-10" id="time${i}"></div>
+        <i class="fas fa-save col-1 saveBtn"></i>
+      </div>`);
+
+    colorCalendarBlocks();
+  }
 }
-
-function createCalendar() {}
 
 function init() {
   setToday();
   createCalendar();
+}
+
+function setToday() {
+  var topDate = now.format("dddd, MMMM Do");
+  currentDayEl.addClass("time-block");
+  currentDayEl.text(topDate);
+}
+
+function colorCalendarBlocks() {
+  if (i === now.hour()) {
+    $(`#time${i}`).addClass("present");
+  } else if (i > now.hour()) {
+    $(`#time${i}`).addClass("future");
+  } else {
+    $(`#time${i}`).addClass("past");
+  }
 }
 
 init();
