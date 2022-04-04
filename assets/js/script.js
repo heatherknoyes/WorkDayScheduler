f49def09d4b664cf5bb9be44c264e351dd469710
Heather Noyes <heatherknoyes@gmail.com>
2022-04-03 21:48:21 -0400

    Added an event to the local storage when the save button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var currentDayEl = $("#currentDay");
 var calendarEl = $(".container");
+var saveBtn = $(".saveBtn");
+var lineItemEl = $(".lineItem");
 var now = moment().local();
+var calendarItems = [];
 var startTime = 9;
 var endTime = 17;
 
@@ -8,14 +11,15 @@ function createCalendar() {
   for (var i = startTime; i <= endTime; i++) {
     var hour = moment(i, "H").format("hA");
 
-    calendarEl.append(`<div class="row">
+    calendarEl.append(`<div class="row lineItem">
         <div class="col-1 hour">${hour}</div>
-        <input class="col-10 textArea" id="time${i}"></input>
+        <input class="col-10 textArea event" id="time${i}"></input>
         <i class="fas fa-save col-1 saveBtn"></i>
       </div>`);
 
     colorCalendarBlocks(i);
   }
+  fillCalendarEvents();
 }
 
 function init() {
@@ -29,6 +33,15 @@ function setToday() {
   currentDayEl.text(topDate);
 }
 
+function fillCalendarEvents() {
+  var calendarItems = localStorage.getItem("calendarItems");
+  if (calendarItems) {
+    var calendarItemsArray = JSON.parse(calendarItems);
+
+    console.log(calendarItemsArray);
+  }
+}
+
 function colorCalendarBlocks(i) {
   if (i === now.hour()) {
     $(`#time${i}`).addClass("present");
@@ -38,5 +51,23 @@ function colorCalendarBlocks(i) {
     $(`#time${i}`).addClass("past");
   }
 }
+
+lineItemEl.on("focus", function (event) {
+  var target = event.target;
+});
+
+$(".container").on("click", ".saveBtn", function (event) {
+  var target = $(event.currentTarget);
+  var rowItem = target.parent();
+  var dayEvent = {
+    hour: rowItem.children(".hour").text(),
+    event: rowItem.children(".event").val(),
+  };
+
+  calendarItems.push(dayEvent);
+  console.log(calendarItems);
+
+  localStorage.setItem("calendarItems", JSON.stringify(calendarItems));
+});
 
 init();
